28928f188a35a00507d98f63fddc3d87386d2023
lacari3ff <42448960+lacari3ff@users.noreply.github.com>
2019-05-09 11:41:49 +0200

    Added the log model

---
+++ models/users/logModel.js
@@ -0,0 +1,13 @@
+module.exports = class Log {
+    constructor(
+        logObject
+    ) {
+        this.log_hid = logObject.log_hid;
+        this.log_key = logObject.log_key;
+        this.log_ip = logObject.log_ip;
+        this.log_date = logObject.log_date;
+
+    }; static save(dbo, cb) {
+        dbo
+    }
+}

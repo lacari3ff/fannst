a1a80aedcca4737821fbe7824801608a406259c9
lacari3ff <42448960+lacari3ff@users.noreply.github.com>
2019-05-09 15:38:04 +0200

    Update login.js

--- public/js/main/login.js
+++ public/js/main/login.js
@@ -1,4 +1,5 @@
 const user = document.getElementById('user');
+const user_pass = document.getElementById('user_pass');
 // Get doms
 const login_sec1 = document.getElementById('login-sec1');
 const login_sec2 = document.getElementById('login-sec2');
@@ -8,9 +9,9 @@ function sec1_finish() {
     if(
         check_user_user()
     ) {
-        //Gets values
+        // Gets values
         var user_user_value = document.getElementById('user').value;
-        //Sends data
+        // Sends data
         var http = new XMLHttpRequest();
         http.open('POST', '/signin-validate');
         http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
@@ -30,7 +31,7 @@ function sec1_finish() {
                         login_sec2.style.animation = 'animate-in .4s both';
                     }, 400)
                 } else {
-                    user.classList.add('input-in-err');
+                    user_not_found();
                 }
             }
         }
@@ -38,7 +39,49 @@ function sec1_finish() {
 }
 
 function sec2_finish() {
-    
+    if(
+        check_user_pass()
+    ) {
+        // Gets values
+        var user_user_value = document.getElementById('user').value;
+        var user_pass_value = document.getElementById('user_pass').value;
+        // Sends data
+        var http = new XMLHttpRequest();
+        http.open('POST', '/signin');
+        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        http.send(
+            'user='+user_user_value+'&user_pass='+user_pass_value
+        );
+        http.onload = function() {
+            if(http.readyState == 4 && http.status == 200) {
+                var data = JSON.parse(http.responseText);
+                if(data.status == true) {
+                    window.location.href = '/';
+                } else {
+                    invalid_password();
+                }
+            }
+        }
+    }
+}
+
+function invalid_password() {
+
+}
+
+function user_not_found() {
+
+}
+
+function check_user_pass() {
+    var user_pass_value = document.getElementById('user_pass').value;
+    user_pass.classList.remove('input-in-err');
+    if(user_pass_value!='') {
+        return true;
+    } else {
+        user_pass.classList.add('input-in-err');
+        return false;
+    }
 }
 
 function check_user_user() {
